e1fe2bbe7a78e4b81f785aacbf1c968172b162a4
hhoury <hhoury@hotmail.com>
2022-03-14 13:06:37 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Chart!"),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('List of TRX'),
+            )
+          ],
+        ));
+  }
+}
